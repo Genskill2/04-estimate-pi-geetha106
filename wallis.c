c6923526e481491390405d3a00c8cb115949b6fc
geetha106 <84716877+geetha106@users.noreply.github.com>
2021-06-04 11:33:26 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -23,16 +23,18 @@ int main(void) {
     }
   }
 }
-float mc_pi(int n)
+float wallis_pi(int n)
 {
-int circle=0;
-float x,y;
-for(int i=1;i<=n;i++)
-  {
-    x=frandom();
-    y=frandom();
-    if(x*x + y*y <= 1){circle++;}
-  }
-  return 4*((float)circle/n);
+int i;
+float pi=1,numerator,denominator,temp;
+for(i=1;i<=n;i++)
+ {
+  numerator=(4*(i*i));
+  denominator=(4*(i*i)-1);
+  temp=numerator/denominator;
+  pi=pi*temp;
+ }
+ return 2*pi;
 }
+
 
